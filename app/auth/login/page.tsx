c488b3a9a45213832e2fa0d43d419b7e0bcb3006
Yassir aittata <yassir.aittata@gmail.com>
2024-01-07 16:31:16 +0000

    create login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Login = () => {
   return (
-    <section className="mt-20">
-      <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+    <section className="mt-8">
+      <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0 ">
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-lg xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-700 md:text-2xl">
               Sign in to your account
@@ -63,14 +63,14 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-secodary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white bg-secodary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <Link
-                  href="/signup"
+                  href="/auth/signup"
                   className="font-semibold text-base text-primary hover:underline"
                 >
                   Sign up
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -77,37 +77,20 @@ const Signup = () => {
                   required
                 />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-blue-500">
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-              </div>
+
               <button
                 type="submit"
-                className="w-full text-white bg-secodary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white bg-secodary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Sign in
+                Sign up
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
+                Already have an account ?{" "}
                 <Link
-                  href="/login"
+                  href="/auth/login"
                   className="font-semibold text-base text-primary hover:underline"
                 >
-                  Sign up
+                  Login
                 </Link>
               </p>
             </form>
