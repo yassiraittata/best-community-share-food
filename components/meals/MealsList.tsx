8173735bb9c955c248335edc9da8a1cd0ab6ffb3
Yassir aittata <yassir.aittata@gmail.com>
2024-01-08 20:07:32 +0000

    add a slide

--- components/meals/MealsList.tsx
+++ components/meals/MealsList.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import MealItem from "./MealItem";
 
 const MealsList = () => {
@@ -7,14 +15,39 @@ const MealsList = () => {
       <h2 className="text-7xl font-extrabold text-secodary/30 font-oswald uppercase">
         Popular meals
       </h2>
-      <ul className="mt-5 grid grid-cols-2 gap-x-4 gap-y-8">
-        <MealItem />
-        <MealItem />
-        <MealItem />
-        <MealItem />
-        <MealItem />
-        <MealItem />
-        <MealItem />
+      <ul className="mt-5">
+        <Swiper
+          slidesPerView={2.4}
+          spaceBetween={30}
+          pagination={{
+            type: "fraction",
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="w-full"
+        >
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MealItem />
+          </SwiperSlide>
+        </Swiper>
       </ul>
     </section>
   );
